d1c5461c299ea4f51b36b8e71abfab7eb77de66f
HUMAM7 <hmamallwany75@gmail.com>
2026-01-06 07:01:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
+import asyncio
 from dashboard import app, tg_app
+async def start_all():
+    await tg_app.initialize()
+    await tg_app.start()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
 
-    tg_app.initialize()
-    tg_app.start()
+    # تشغيل Telegram Application بشكل صحيح
+    asyncio.run(start_all())
 
+    # تشغيل Flask
     app.run(
         host="0.0.0.0",
         port=port,
