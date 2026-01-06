46f00ef0116bfcc1726c3b839380f0cb9c74771a
HUMAM7 <hmamallwany75@gmail.com>
2026-01-06 05:27:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import threading
 import os
 
-# تشغيل البوت
 def run_bot():
     import bot
     bot.main()
 
-# تشغيل لوحة التحكم
 def run_dashboard():
     from dashboard import app
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
-    t1 = threading.Thread(target=run_bot)
-    t1.start()
+    t = threading.Thread(target=run_bot)
+    t.daemon = True          # ⭐⭐⭐ هذا السطر هو الحل
+    t.start()
 
     run_dashboard()
